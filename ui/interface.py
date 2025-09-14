adf8ba434bdb1234d35e26956556aeab54449b4d
danesm03 <danesmithhh@gmail.com>
2025-09-13 19:13:01 -0500

    Added barebones GenerateScreen page ui.

--- ui/interface.py
+++ ui/interface.py
@@ -1,15 +1,36 @@
 from textual.app import App, ComposeResult
 from textual.containers import ScrollableContainer, Vertical, Horizontal, Container
-from textual.widgets import Footer, Header, Static, Button
+from textual.widgets import Footer, Header, Static, Button, Input
 from textual.screen import Screen
+from textual import on
 
 class GenerateScreen(Screen):
-    pass
+    BINDINGS = [
+        ("ctrl+d", "toggle_dark_mode", "Toggle Dark Mode"),#("return", "app.push_screen(GenerateScreen)", "Start")
+    ]    
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
+        yield Input(
+            placeholder="Enter Japanese here ...",
+            type="text",
+            max_length=25000,
+            id="generate_text_input"
+        )
+        yield Horizontal(
+            Button("Saved Cards", id="saved-cards-btn", variant="primary"),
+            Button("Generate", id="generate-text-btn", variant="primary"),
+            Button("Saved Texts", id="saved-texts-btn", variant="primary"),
+            id="gentextscrn-cntnr",
+        )
+
 
 class StartScreen(Screen):
     BINDINGS = [
         ("d", "toggle_dark_mode", "Toggle Dark Mode"),#("return", "app.push_screen(GenerateScreen)", "Start")
     ]
+
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -25,13 +46,13 @@ class StartScreen(Screen):
     | $$ \  $$|  $$$$$$/   | $$   |  $$$$$$/| $$$$$$$/|  $$$$$$/| $$$$$$$/| $$$$$$$/    | $$    
     |__/  \__/ \______/    |__/    \______/ |_______/  \______/ |_______/ |_______/     |__/   
     """, id="title"),
-    Static("[bold]コトバーディ[/bold]", id="kotobuddy_subtitle"),
-    Horizontal(
-        Button("Start", id="start-btn", variant="default"),
-        id="start-btn-container",
-    ),
-    id="start-screen"
-)
+        Static("[bold]コトバディ[/bold]", id="kotobuddy_subtitle"),
+        Horizontal(
+            Button("Start", id="start-btn", variant="primary" ),
+            id="start-btn-container",
+        ),
+        id="start-screen"
+    )
 
 
 
@@ -48,6 +69,7 @@ class KotobuddyApp(App):
 
     CSS_PATH = "kotobuddy.tcss"
 
+
     def compose(self):
 
         """What widgets is this app composed of?"""
@@ -62,6 +84,10 @@ class KotobuddyApp(App):
     
     def on_mount(self):
         self.push_screen(StartScreen())
+
+    @on(Button.Pressed, "#start-btn")
+    def start_kotobuddy(self):
+        self.push_screen(GenerateScreen())
 
     
 
